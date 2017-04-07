f37e56ffdcf2d51b2e5d8715a1ae47d22d0a8a5c
Eduard Ungureanu <u.eduard.email@gmail.com>
2017-04-07 11:35:58 +0300

    adding timeout
    elements are not ready when the code runs, we need to add a delay

--- cta-swap-title-image.js
+++ cta-swap-title-image.js
@@ -1,12 +1,15 @@
 <script>
-var cta = jQuery('.wpc-cta');
-cta.each(function(){
-	var cta_wrapper = jQuery(this).find('.et_pb_promo_description'),
-		cta_title = jQuery(this).find('h2'),
-		cta_image = jQuery(this).find('h1');
-	cta_title.detach();
-	cta_image.detach();
-	cta_wrapper.prepend(cta_image);
-	cta_wrapper.append(cta_title);
-});
+function swap_title_image(){
+	var cta = jQuery('.wpc-cta');
+	cta.each(function(){
+		var cta_wrapper = jQuery(this).find('.et_pb_promo_description'),
+			cta_title = jQuery(this).find('h2'),
+			cta_image = jQuery(this).find('h1');
+		cta_title.detach();
+		cta_image.detach();
+		cta_wrapper.prepend(cta_image);
+		cta_wrapper.append(cta_title);
+	});
+}
+setTimeout(swap_title_image, 100);
 </script>
