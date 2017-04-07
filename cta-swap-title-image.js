bc2daf347068495a6a7d58cc864fa476be20d573
Eduard Ungureanu <u.eduard.email@gmail.com>
2017-04-07 11:21:53 +0300

    CTA Module
    Swap the title with the image

---
+++ cta-swap-title-image.js
@@ -0,0 +1,12 @@
+<script>
+var cta = jQuery('.wpc-cta');
+cta.each(function(){
+	var cta_wrapper = jQuery(this).find('.et_pb_promo_description'),
+		cta_title = jQuery(this).find('h2'),
+		cta_image = jQuery(this).find('h1');
+	cta_title.detach();
+	cta_image.detach();
+	cta_wrapper.prepend(cta_image);
+	cta_wrapper.append(cta_title);
+});
+</script>
