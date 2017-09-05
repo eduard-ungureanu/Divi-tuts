37c750c289475fcd1421de3a49aa3a16c046d014
Eduard Ungureanu <u.eduard.email@gmail.com>
2017-09-05 23:54:59 +0300

    adding readme file

--- BlogModules/Scrolling-Posts/functions.php
+++ BlogModules/Scrolling-Posts/functions.php
@@ -1,11 +1,10 @@
-<?php
 /*================================================
 #Load custom Blog Module
 ================================================*/
 function divi_child_theme_setup() {
-	get_template_part( 'custom-modules/cbm1' );
-	$cbm1 = new WPC_ET_Builder_Module_Blog();
+	get_template_part( 'custom-modules/cbm' );
+	$cbm = new WPC_ET_Builder_Module_Blog();
 	remove_shortcode( 'et_pb_blog' );
-	add_shortcode( 'et_pb_blog', array($cbm1, '_shortcode_callback') );
+	add_shortcode( 'et_pb_blog', array($cbm, '_shortcode_callback') );
 }
 add_action('et_builder_ready', 'divi_child_theme_setup' );
---
+++ BlogModules/Scrolling-Posts/readme.md
@@ -0,0 +1,7 @@
+## How to use the files
+
+1. download the `transparend.png` file from the images folder
+2. upload the `transparent.png` file to your `child theme folder > images`
+3. in the `custom-modules` folder create a new folder called `cbm.php`
+4. in the `cbm.php` file add the code from `cbm.php` file above
+5. in `functions.php` put the code from `functions.php` file above
